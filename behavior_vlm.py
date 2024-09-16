d3267a0439aed611a4b05b984ef67cdeae5f0926
nguyendang16 <nguyendk0106@gmail.com>
2024-09-16 20:28:02 +0700

    modify code

--- behavior_vlm.py
+++ behavior_vlm.py
@@ -7,7 +7,6 @@ from PIL import Image
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model, preprocess = clip.load("ViT-B/32", device=device)
 
-# Danh sách mô tả hành vi
 descriptions = [
     "a person sitting",
     "a person walking",
@@ -19,27 +18,25 @@ descriptions = [
     "a person standing"
 ]
 
-# Tokenize các mô tả hành vi
+# Tokenize behavior description
 text_inputs = clip.tokenize(descriptions).to(device)
 
-# Hàm nhận diện hành vi
 def detect_behavior(frame):
     # Preprocess frame
     image = preprocess(Image.fromarray(frame)).unsqueeze(0).to(device)
     
-    # Dự đoán hành vi sử dụng CLIP
+    # predict behavior using clip
     with torch.no_grad():
         logits_per_image, logits_per_text = model(image, text_inputs)
         probs = logits_per_image.softmax(dim=-1).cpu().numpy()
 
-    # Lấy hành vi có xác suất cao nhất
+    # take the highest behavior's accuracy
     predicted_behavior = descriptions[probs.argmax()]
     return predicted_behavior
 
-# Sử dụng OpenCV để mở video
-cap = cv2.VideoCapture(0)  # Dùng webcam hoặc video path
+cap = cv2.VideoCapture(0)  
 
-frame_skip = 5  # Số lượng khung hình bỏ qua giữa mỗi lần xử lý
+frame_skip = 5  # frame skipped to process 
 frame_count = 0
 
 while True:
@@ -50,17 +47,12 @@ while True:
     frame = cv2.flip(frame, 1) 
     resized_frame = cv2.resize(frame, (224, 224))
     
-    # Chỉ xử lý 1 khung hình sau mỗi frame_skip khung hình để giảm tải
     if frame_count % frame_skip == 0:
-        # Nhận diện hành vi
         behavior = detect_behavior(resized_frame)
     
     frame_count += 1
     
-    # Hiển thị hành vi lên video
     cv2.putText(frame, f"Behavior: {behavior}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
-    
-    # Hiển thị video
     cv2.imshow('Real-time Behavior Detection', frame)
     
     if cv2.waitKey(1) & 0xFF == ord('q'):
